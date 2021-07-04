31296635103c3079d883396636ac2df7c88e251e
Ryang Sohn <loop.infinitely@gmail.com>
2021-07-04 22:46:31 +0900

    Add PostView for viewing posts

---
+++ src/components/PostView.tsx
@@ -0,0 +1,62 @@
+import { createStyles, makeStyles, Theme, Typography } from "@material-ui/core";
+import MainAppBar from "./MainAppBar";
+import PropTypes, { InferProps } from "prop-types";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Post } from "../post";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(2),
+    },
+  })
+);
+
+interface ParamTypes {
+  postId: string;
+}
+
+export default function PostView({
+  token,
+}: InferProps<typeof PostView.propTypes>) {
+  const classes = useStyles();
+  const { postId } = useParams<ParamTypes>();
+  const id: number = +postId;
+  const [post, setPost] = useState({} as Post);
+  useEffect(() => {
+    async function getPost() {
+      const res = await axios.get(
+        `${process.env.REACT_APP_API_URI}/post/get/${id}`,
+        { headers: { Authorization: token } }
+      );
+      setPost({
+        id: id,
+        title: res.data["title"],
+        projectName: res.data["project_name"],
+        authorName: res.data["author_name"],
+        creationDate: res.data["creation_date"],
+        content: res.data["content"],
+      });
+    }
+    getPost();
+  }, [token, id]);
+  return (
+    <>
+      <MainAppBar />
+      <main className={classes.content}>
+        <Typography variant="h1">{post.title}</Typography>
+        <Typography variant="subtitle1">
+          {post.projectName} | {post.authorName} | {post.creationDate}
+        </Typography>
+        <Typography variant="body1">{post.content}</Typography>
+      </main>
+    </>
+  );
+}
+
+PostView.propTypes = {
+  token: PropTypes.string.isRequired,
+};
--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,11 +1,17 @@
 import PropTypes, { InferProps } from "prop-types";
 import { Route, Switch } from "react-router-dom";
 import PostList from "./PostList";
+import PostView from "./PostView";
 
 export default function Routes({ token }: InferProps<typeof Routes.propTypes>) {
   return (
     <Switch>
       <Route exact path="/" render={() => <PostList token={token} />} />
+      <Route
+        exact
+        path="/post/:postId"
+        render={() => <PostView token={token} />}
+      />
     </Switch>
   );
 }
