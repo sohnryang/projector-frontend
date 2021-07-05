501321febac09d4bbbadb1020b72a121a727bdbc
Ryang Sohn <loop.infinitely@gmail.com>
2021-07-05 23:05:14 +0900

    Add search bar

--- src/components/MainAppBar.tsx
+++ src/components/MainAppBar.tsx
@@ -2,11 +2,14 @@ import {
   AppBar,
   Button,
   createStyles,
+  fade,
+  InputBase,
   makeStyles,
   Theme,
   Toolbar,
   Typography,
 } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -28,6 +31,43 @@ const useStyles = makeStyles((theme: Theme) =>
         duration: theme.transitions.duration.leavingScreen,
       }),
     },
+    search: {
+      position: "relative",
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      "&:hover": {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(3),
+        width: "auto",
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: "100%",
+      position: "absolute",
+      pointerEvents: "none",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    inputRoot: {
+      color: "inherit",
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("md")]: {
+        width: "20ch",
+      },
+    },
   })
 );
 
@@ -41,6 +81,16 @@ export default function MainAppBar() {
           <Typography variant="h6" className={classes.title}>
             Projector
           </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search..."
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
           <Button color="inherit">Logout</Button>
         </Toolbar>
       </AppBar>
