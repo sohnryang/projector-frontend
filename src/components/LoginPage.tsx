febe9e6e664cbc3e6e1098500ddc1a3bf905aaa9
Ryang Sohn <loop.infinitely@gmail.com>
2021-07-10 15:58:18 +0900

    Polish login page

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -3,6 +3,7 @@ import GoogleLogin from "react-google-login";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
 import { User } from "../user";
+import TypeWriter from "typewriter-effect";
 
 export default function LoginPage({
   token,
@@ -26,11 +27,51 @@ export default function LoginPage({
   if (token !== "" && (user as User).id !== 0) return <Redirect to="/" />;
 
   return (
-    <GoogleLogin
-      clientId="732283162920-fo65na0l33ahj1f02a6qdnhcg3hdq29n.apps.googleusercontent.com"
-      onSuccess={handleResponse}
-      buttonText="Login"
-    />
+    <div
+      style={{
+        display: "flex",
+        flexFlow: "column",
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        alignItems: "center",
+      }}
+    >
+      <span
+        style={{
+          fontFamily: "Major Mono Display",
+          fontSize: "4.5em",
+        }}
+      >
+        Projector
+      </span>
+      <br />
+      <TypeWriter
+        onInit={(typewriter) => {
+          typewriter
+            .typeString("A project site ")
+            .typeString("of HAS students")
+            .pauseFor(2000)
+            .deleteChars(15)
+            .typeString("by HAS student")
+            .pauseFor(2000)
+            .deleteChars(14)
+            .typeString("for HAS students")
+            .pauseFor(2000)
+            .deleteChars(16)
+            .start();
+        }}
+        options={{ loop: true, autoStart: true }}
+      />
+      <div style={{ flexShrink: 0, flexGrow: 0, marginTop: "1.5em" }}>
+        <GoogleLogin
+          clientId="732283162920-fo65na0l33ahj1f02a6qdnhcg3hdq29n.apps.googleusercontent.com"
+          onSuccess={handleResponse}
+          buttonText="Login"
+        />
+      </div>
+    </div>
   );
 }
 
